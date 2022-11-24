3f2ee7fcae35703573b8ce5d910a2748f6ecd970
sebastiann88 <sebastian.nadeau45@gmail.com>
2022-11-24 06:33:31 -0500

    add giff

--- script.js
+++ script.js
@@ -53,3 +53,14 @@ icon.onclick = function() {
     icon.src = "media/play.png"
   }
 }
+
+// Giff
+
+function hideLoader() {
+    $('#loading').hide();
+}
+
+$(window).ready(hideLoader);
+
+// Strongly recommended: Hide loader after 20 seconds, even if the page hasn't finished loading
+setTimeout(hideLoader, 20 * 1000);
