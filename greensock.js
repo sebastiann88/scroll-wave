5cfd3e4775713272b5d5e818019bc34abbf82931
sebastiann88 <sebastian.nadeau45@gmail.com>
2022-11-24 09:53:36 -0500

    make changes

--- greensock.js
+++ greensock.js
@@ -3,7 +3,7 @@
 gsap.to('#image-sequence', {
 	scrollTrigger: {
 		trigger: ".animation-fade-out",
-		toggleActions: "play none none none"
+		toggleActions: "play none none reverse"
 	},
  	opacity: 0,
  	duration: 2,
@@ -12,7 +12,7 @@ gsap.to('#image-sequence', {
 gsap.to('.play-button-container', {
 	scrollTrigger: {
 		trigger: ".animation-fade-out",
-		toggleActions: "play none none none"
+		toggleActions: "play none none reverse"
 	},
  	opacity: 0,
  	duration: 2,
@@ -20,22 +20,32 @@ gsap.to('.play-button-container', {
 
 // Portfolio
 
-gsap.to('.portfolio', {
+gsap.to('.portfolio-button-link', {
 	scrollTrigger: {
-		trigger: ".portfolio-fade-in",
-		toggleActions: "play none none none"
+		trigger: ".portfolio-fade-in-link",
+		toggleActions: "play none none reverse",
 	},
  	opacity: 1,
  	duration: 2,
- 	delay: 2.15
+ 	// delay: 4.15
+})
+
+gsap.to('.portfolio', {
+	scrollTrigger: {
+		trigger: ".portfolio-fade-in",
+		toggleActions: "play none none none",
+	},
+ 	opacity: 1,
+ 	duration: 2,
+ 	delay: 0.10
 })
 
 gsap.to('#container', {
 	scrollTrigger: {
 		trigger: ".portfolio-fade-in",
-		toggleActions: "play none none none"
+		toggleActions: "play none none none",
 	},
  	opacity: 1,
  	duration: 2,
- 	delay: 2.15
+ 	delay: 0.10
 })
--- script.js
+++ script.js
@@ -56,25 +56,12 @@ icon.onclick = function() {
 
 // Giff
 
-let loader;
 
-function loadNow(opacity) {
-    if (opacity <= 0) {
-        displayContent();
-    } else {
-        loader.style.opacity = opacity;
-        window.setTimeout(function() {
-            loadNow(opacity - 0.05);
-        }, 50);
-    }
-}
 
-function displayContent() {
-    loader.style.display = 'none';
-    document.getElementById('content').style.display = 'block';
-}
+// Scroll to Top
 
-document.addEventListener("DOMContentLoaded", function() {
-    loader = document.getElementById('loader');
-    loadNow(1);
-});
+// window.scroll({
+//   top: 0, 
+//   left: 0, 
+//   behavior: 'smooth',
+// });
